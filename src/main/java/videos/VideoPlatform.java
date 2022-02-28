81c5f8bee099624a25d1f1e7d1eead8bfdf11f38
codriv <combodriver4@yahoo.at>
2022-02-28 11:17:17 +0100

    sv2-2021-jvjbf-zarovizsga - VideoPlatform

---
+++ src/main/java/videos/VideoPlatform.java
@@ -0,0 +1,37 @@
+package videos;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class VideoPlatform {
+
+    private List<Channel> channels = new ArrayList<>();
+
+    public List<Channel> getChannels() {
+        return channels;
+    }
+
+    public void readDataFromFile(Path path) {
+        try(Scanner scanner = new Scanner(path)) {
+            scanner.nextLine();
+            while (scanner.hasNext()) {
+                String line = scanner.nextLine();
+                String[] parts = line.split(";");
+                String channelName = parts[0];
+                int subscriptions = Integer.parseInt(parts[1]);
+                int numberOfVideos = Integer.parseInt(parts[2]);
+                channels.add(new Channel(channelName, subscriptions, numberOfVideos));
+            }
+        } catch (IOException ioe) {
+            throw new IllegalArgumentException("Cannot open file for read!");
+        }
+    }
+
+    public int calculateSumOfVideos() {
+        return channels.stream().flatMapToInt(c -> IntStream.of(c.getNumberOfVideos())).sum();
+    }
+}
